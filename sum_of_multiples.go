1898309f288939f0133bfbb8990f074ce9dced7b
shane <shanecandoit@gmail.com>
2020-01-21 10:33:25 -0600

    remove prints

--- sum_of_multiples.go
+++ sum_of_multiples.go
@@ -1,10 +1,5 @@
 package summultiples
 
-import (
-	"fmt"
-	"strconv"
-)
-
 // SumMultiples given limit, find the sum of all the unique multiples of particular numbers up to but not including that number
 func SumMultiples(limit int, divisors ...int) (sum int) {
 
@@ -12,11 +7,11 @@ func SumMultiples(limit int, divisors ...int) (sum int) {
 	// we get 3, 5, 6, 9, 10, 12, 15, and 18.
 	// The sum of these is 78.
 
-	fmt.Println("limit", limit)
-	fmt.Println("divisors", divisors)
+	// fmt.Println("limit", limit)
+	// fmt.Println("divisors", divisors)
 
 	sum = 0
-	mults := ""
+	// mults := ""
 
 	for i := 0; i < limit; i++ {
 		for _, num := range divisors {
@@ -25,22 +20,22 @@ func SumMultiples(limit int, divisors ...int) (sum int) {
 				continue
 			}
 			if i%num == 0 {
-				mults += strconv.Itoa(i) + " "
+				// mults += strconv.Itoa(i) + " "
 				sum += i
 				break
 			}
 		}
 	}
 
-	fmt.Println("mults", mults)
-	fmt.Println("sum", sum)
+	// fmt.Println("mults", mults)
+	// fmt.Println("sum", sum)
 	return sum
 }
 
 func main() {
 
 	twenty := SumMultiples(20, 3, 5)
-	if twenty != 78 {
+	if twenty != 77 {
 		panic("20 -> 78")
 	}
 }
